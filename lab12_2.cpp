a3dacbdd3f825d80a73f560eae0817002936b877
Mac <mac@Macs-MacBook-Pro.local>
2020-02-07 16:57:45 +0700

    done

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -15,7 +15,7 @@ int main()
     int s,x,y;
     do{
         showImage(image);
-        cout << "Input your brush size and location:";
+        cout << "\nInput your brush size and location:";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
     }while(s != 0 && x != 0 && y != 0 );
@@ -23,3 +23,29 @@ int main()
 }
 
 // Write definition of updateImage() and showImage() here
+void showImage(const bool image[N][M])
+{
+    for(int i=0 ; i<M+2;i++) cout << "-";
+    cout << "\n";
+    for(int i =0;i<N;i++){
+        cout << "|";
+        for(int j = 0; j<M;j++)
+        {
+            if(image[i][j] == 1) cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+        cout <<"\n";
+    }
+    for (int i = 0;i<M+2;i++) cout << "-";
+}
+void updateImage(bool image[N][M],int s ,int x,int y)
+{
+    for(int i = 0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if (sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1) image[i][j] =1;
+        }
+    }
+}
